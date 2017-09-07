b49de15b909595c12941bd8cf2717246d62b27bf
knm0727 <>
2017-09-07 13:43:44 -0400

    checkpoint-25: almost complete

--- scripts/album.js
+++ scripts/album.js
@@ -30,31 +30,48 @@
      ]
  };
 
- var createSongRow = function(songNumber, songName, songLength) {
-     var template =
-        '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
-      + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
-      + '</tr>'
-      ;
-
-     return template;
+ // 4th Album (assignment 24)
+ var albumKmills = {
+     title: 'Self Made',
+     artist: 'K MILLS',
+     label: 'Akata',
+     year: '2018',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Maddox', duration: '8:26' },
+         { title: 'Lincoln', duration: '10:44' },
+         { title: 'Benny', duration: '12:00'},
+         { title: 'Ndurache', duration: '4:03' },
+         { title: 'Fab4', duration: '4:44'}
+     ]
  };
 
- var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+      '<tr class="album-view-song-item">'
+      '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+      '  <td class="song-item-title">' + songName + '</td>'
+      '  <td class="song-item-duration">' + songLength + '</td>'
+      '</tr>'
+     ;
 
-    // #2
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
+     return template;
+  };
+
+  // #1
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+
+var setCurrentAlbum = function(album) {
+     // #2
+ albumTitle.firstChild.nodeValue = album.title;
+ albumArtist.firstChild.nodeValue = album.artist;
+ albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+ albumImage.setAttribute('src', album.albumArtUrl);
 
     // #3
     albumSongList.innerHTML = '';
@@ -64,7 +81,39 @@
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event) {
+      // Only target individual song rows during event delegation
+          if (event.target.parentElement.className === 'album-view-song-item') {
+          event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+          }
+        });
+
+        for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+          // Selects first child element, which is the song-item-number element
+          this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+      }
+
+//MAYBE }
+
+
+var ablums = [albumPicasso, albumMarconi, albumKmills];
+var index = 1;
+albumImage.addEventListener("click", function(event) {
+    setCurrentAlbum(albums[index]);
+    index++;
+    if (index == albums.length) {
+       index - 0;
+    }
+  });
 };
