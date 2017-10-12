8c3412d2a9546552684879d84d469b2bc919977c
knm0727 <>
2017-10-12 11:19:03 -0400

    checkpoint-31: updates

--- scripts/album.js
+++ scripts/album.js
@@ -1,52 +1,4 @@
-// Example Album
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
-
- // Another Example Album
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
-
- // 4th Album (assignment 24)
- var albumKmills = {
-     title: 'Self Made',
-     artist: 'K MILLS',
-     label: 'Akata',
-     year: '2018',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Maddox', duration: '8:26' },
-         { title: 'Lincoln', duration: '10:44' },
-         { title: 'Benny', duration: '12:00'},
-         { title: 'Ndurache', duration: '4:03' },
-         { title: 'Fab4', duration: '4:44'}
-     ]
- };
-
- var createSongRow = function(songNumber, songName, songLength) {
+var createSongRow = function(songNumber, songName, songLength) {
    console.log("adding the song template");
      var template =
       '<tr class="album-view-song-item">'
@@ -203,16 +155,17 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
 var playerBarPauseButton = '<span class="ion-pause"></span>';
- // Store state of playing songs
-var currentAlbum = null;
-var currentlyPlayingSongNumber = null;
-var currentSongFromAlbum = null;
+
+
 
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
 
 
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
